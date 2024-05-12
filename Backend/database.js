ffd1bff2ed82258c49a7912d42a7e2d2d13e3d5c
LaljiSankhat <135532699+LaljiSankhat@users.noreply.github.com>
2024-05-12 11:30:38 +0530

    Delete Backend directory

--- Backend/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-
-exports.connectMongoose = () => {
-    mongoose.connect('mongodb://localhost:27017/city')
-    .then((e) => console.log(`Connectd to mongoDB:${e.connection.host}`))
-    .catch((e) => console.log(e));
-};
